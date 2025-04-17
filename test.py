a6bb7e8d244137ca9d1d90376f04f4bbd7941ec0
Nikolas Zagora <nikolas.zagora@gmx.net>
2025-04-17 10:52:55 +0200

    Weiterentwicklung Linien-/Farbsensor

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python3
 from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_D, MoveTank
-from ev3dev2.sensor.lego import TouchSensor, ColorSensor
+from ev3dev2.sensor.lego import TouchSensor, ColorSensor, InfraredSensor
 from ev3dev2.sensor import Sensor
 from time import sleep
 
@@ -8,6 +7,7 @@ from time import sleep
 sensor_touch = TouchSensor()
 sensor_floor = ColorSensor(address='in2')     # EV3 Color Sensor auf Boden
 sensor_right = Sensor(address='in3', driver_name='ht-nxt-color')  # HiTechnic Sensor nach rechts
+sensor_ir = InfraredSensor(address='in4')     # Infrarot-Sensor vorne
 
 # Initialisiere die Motoren
 tank_drive = MoveTank(OUTPUT_A, OUTPUT_D)
@@ -18,53 +18,52 @@ WHITE = 1
 YELLOW = 6
 PURPLE = 7
 BLUE = 2
-GREEN = 3
+GREEN = 4
 RED = 5
 THRESHOLD = (BLACK + WHITE) / 2  # Schwellenwert fuer die Linie
 
-# Raumfarben in Reihenfolge: [Raum 0, Raum 1, Raum 2, Raum 3]
-room_colors = [YELLOW, PURPLE, RED, BLUE]
-
 # Results line following
-TARGET_ROOM_REACHED = "TARGET_REACHED"
+TARGET_ROOM_REACHED = "TARGET_ROOM_REACHED"
 CONTINUE_SEARCH = "CONTINUE_SEARCH"
 
 def driveToRoom():
-    #Roboter fährt in ein Behandlungszimmer oder in das Wartezimmer
-    #Prüfung, ob Handy auf sensor liegt, sonst error Code zurückgeben
-    print("Warte auf 5 Sekunden langen Druck auf den Touchsensor")
-    pressed_time = 0
-    while pressed_time < 5:
-        if sensor_touch.is_pressed:
-            sleep(1)
-            pressed_time += 1
-            print("{} Sekunde(n) gedrueckt".format(pressed_time))
-        else:
-            pressed_time = 0 # Timer zurücksetzen, wenn losgelassen
+    # Roboter fährt in ein Behandlungszimmer oder in das Wartezimmer
+    # Prüfung, ob Handy auf Sensor liegt, sonst Error-Code zurückgeben
+    # Warte auf 5 Sekunden langen Druck auf den Touchsensor
+    # print("Warte auf 5 Sekunden langen Druck auf den Touchsensor")
+    # pressed_time = 0
+    # while pressed_time < 5:
+    #     if sensor_touch.is_pressed:
+    #         sleep(1)
+    #         pressed_time += 1
+    #         print("{} Sekunde(n) gedrueckt".format(pressed_time))
+    #     else:
+    #         pressed_time = 0
 
     print("Startsignal erkannt. Roboter faehrt los")
 
-    # Ziel-Farbwahl durch Benutzer
-    print("Waehle Ziel-Farbe: (1) Blau, (2) Gruen")
-    auswahl = input("Eingabe: ")
-    if auswahl == "1":
-        target_color = BLUE
-    elif auswahl == "2":
-        target_color = GREEN
-    else:
-        print("Ungueltige Auswahl. Standard: Blau")
-        target_color = BLUE
+    # Ziel-Farbwahl durch Benutzer mit Wiederholung bei ungueltiger Eingabe
+    target_color = None
+    while target_color is None:
+        print("Waehle Ziel-Farbe: (1) Blau, (2) Green")
+        auswahl = input("Eingabe: ")
+        if auswahl == "1":
+            target_color = BLUE
+        elif auswahl == "2":
+            target_color = GREEN
+        else:
+            print("Ungueltige Auswahl. Bitte erneut eingeben.")
 
     print("Ziel: Farbe {} - Roboter soll dort abbiegen.".format(target_color))
 
     while True:
-        result = follow_line(target_color, room_colors)
+        result = follow_line(target_color, [YELLOW, PURPLE, RED, BLUE, GREEN])
 
         if result == TARGET_ROOM_REACHED:
             print("Ziel erreicht - nach links abbiegen und Linie suchen")
             turn_left_90_degrees()
             while True:
-                follow_result = follow_line(target_color, room_colors)
+                follow_result = follow_line(target_color, [YELLOW, PURPLE, RED, BLUE, GREEN])
                 if follow_result == TARGET_ROOM_REACHED:
                     print("Zweites Ziel erreicht oder erneut gleiche Farbe erkannt.")
                     break
@@ -72,16 +71,16 @@ def driveToRoom():
 
 def driveToBase():
     # Roboter fährt zu Ausgangspunkt zurück
-    # eventuell prüfen, ob Handy auf dem Roboter liegt, sonst error Code
+    # eventuell prüfen, ob Handy auf dem Roboter liegt, sonst Error-Code
     print("Fahre zu Start")
 
 def PickupPatientFromWaitingRoom():
     # Roboter fährt ins Wartezimmer um Patient abzuholen
-    # Prüfen, ob Handy aufgehoben wurde, sonst error Code zurückgeben
+    # Prüfen, ob Handy aufgehoben wurde, sonst Error-Code zurückgeben
     print("Hole Patient im Wartezimmer ab")
 
 def turn_left_90_degrees():
-     """ Dreht den Roboter nach um 90° nach links, bis er wieder Schwarz erkennt """
+    # Dreht den Roboter um 90° nach links
     print("Drehe 90 Grad nach links")
     tank_drive.on_for_degrees(left_speed=-20, right_speed=20, degrees=200)
     tank_drive.off()
@@ -89,19 +88,32 @@ def turn_left_90_degrees():
 def follow_line(target_color, all_room_colors):
     floor_color = sensor_floor.color
     right_color_id = sensor_right.value(0)
+    distance = sensor_ir.proximity
 
-    print(">>> Bodenfarbe: {}, Rechts erkannt (ID): {}".format(floor_color, right_color_id))
+    print(">>> Bodenfarbe: {}, Rechts erkannt (ID): {}, Distanz: {}".format(floor_color, right_color_id, distance))
 
+    # Wenn Hindernis erkannt wird, stoppe
+    if distance < 30:
+        print("Hindernis erkannt - Roboter stoppt.")
+        tank_drive.off()
+        while sensor_ir.proximity < 30:
+            sleep(0.1)
+        print("Hindernis entfernt - Roboter faehrt weiter.")
+        return CONTINUE_SEARCH
+
+    # Prüfe ob Ziel-Farbblock erkannt wurde
     if right_color_id == target_color:
         print("Ziel-Farbblock erkannt (rechts). Biege ab.")
         tank_drive.off()
         return TARGET_ROOM_REACHED
 
+    # Falsche Raumfarbe erkannt – weiterfahren
     elif right_color_id in all_room_colors:
         print("Falsche Raumfarbe erkannt (rechts), weiterfahren")
         tank_drive.on_for_seconds(left_speed=15, right_speed=15, seconds=1)
         return CONTINUE_SEARCH
 
+    # Linienverfolgung basierend auf Bodenfarbe
     if floor_color == BLACK:
         tank_drive.on(left_speed=10, right_speed=15)
     elif floor_color == WHITE:
@@ -111,6 +123,7 @@ def follow_line(target_color, all_room_colors):
     return CONTINUE_SEARCH
 
 def main():
+    # Startpunkt des Programms
     print("Druecke den Touch-Sensor fuer 5 Sekunden, um den Roboter starten zu lassen.")
     driveToRoom()
 
