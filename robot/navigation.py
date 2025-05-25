c00505f6519f69f50505207418c2bfd0e9ccb3a2
Nikolas Zagora <nikolas.zagora@gmx.net>
2025-05-26 00:54:59 +0200

    hardware.py refactoring

--- robot/navigation.py
+++ robot/navigation.py
@@ -1,5 +1,5 @@
 from time import sleep
-from robot.hardware import *
+from robot.hardware import ev3_hardware, ColorValues, SpeedConstants, THRESHOLD
 
 # Results line following
 TARGET_ROOM_REACHED = "TARGET_ROOM_REACHED"
@@ -16,27 +16,26 @@ positionRobot = POSITION_START
 
 def check_and_handle_obstacle(threshold = THRESHOLD):
     """Überprüft auf Hindernis und pausiert bei Bedarf"""
-    distance = sensor_ir.proximity
+    distance = ev3_hardware.sensor_ir.proximity
     if distance < threshold:
         print("Hindernis erkannt - Roboter stoppt.")
-        tank_drive.off()
-        while sensor_ir.proximity < threshold:  # Warte bis Hindernis entfernt ist
+        ev3_hardware.tank_drive.off()
+        while ev3_hardware.sensor_ir.proximity < threshold:  # Warte bis Hindernis entfernt ist
             sleep(0.5)
         print("Hindernis entfernt - Roboter faehrt weiter.")
     return
-
 
 def follow_line_simple(floor_color=None):
     """Einfache Linienverfolgung basierend auf der Bodenfarbe."""
     check_and_handle_obstacle()
 
-    if floor_color == BLACK or floor_color == NONE:
-        tank_drive.on(left_speed=SPEED_LINE_BLACK_L, right_speed=SPEED_LINE_BLACK_R)
-    elif floor_color == WHITE:
-        tank_drive.on(left_speed=SPEED_LINE_WHITE_L, right_speed=SPEED_LINE_WHITE_R)
+    if floor_color == ColorValues.BLACK or floor_color == ColorValues.NONE: 
+        ev3_hardware.tank_drive.on(left_speed=SpeedConstants.LINE_BLACK_L, right_speed=SpeedConstants.LINE_BLACK_R) 
+    elif floor_color == ColorValues.WHITE: 
+        ev3_hardware.tank_drive.on(left_speed=SpeedConstants.LINE_WHITE_L, right_speed=SpeedConstants.LINE_WHITE_R) 
     else:
         # Bei anderen Farben (z.B. Rand der Linie, oder unerwartete Farbe) geradeaus fahren oder anpassen
-        tank_drive.on(left_speed=SPEED_LINE_OTHER, right_speed=SPEED_LINE_OTHER)
+        ev3_hardware.tank_drive.on(left_speed=SpeedConstants.LINE_OTHER, right_speed=SpeedConstants.LINE_OTHER) 
 
 
 def _get_target_index(rooms):
@@ -61,8 +60,8 @@ def _navigate_in_target_room(target_index, ws=None):
 
             # Folge der Linie bis zur blauen Platte im Raum
             while True:
-                floor_color = sensor_floor.color
-                right_color_id = sensor_right.value(0)
+                floor_color = ev3_hardware.sensor_floor.color 
+                right_color_id = ev3_hardware.sensor_right.value(0) 
 
                 # print(">>> Nach dem Abbiegen - Bodenfarbe: {}, Rechts erkannt (ID): {}, Distanz: {}".format(floor_color, right_color_id, distance))
 
@@ -70,15 +69,15 @@ def _navigate_in_target_room(target_index, ws=None):
                     sleep(0.1)
                     continue
 
-                elif right_color_id == BLUE:
+                elif right_color_id == ColorValues.BLUE: 
                     print(
                         "Blaue Platte im Raum erkannt - 180 Grad drehen und auf Handy warten"
                     )
-                    tank_drive.off()
-                    tank_drive.on_for_degrees(
-                        left_speed=-SPEED_TURN, right_speed=SPEED_TURN, degrees=406
+                    ev3_hardware.tank_drive.off() 
+                    ev3_hardware.tank_drive.on_for_degrees( 
+                        left_speed=-SpeedConstants.TURN, right_speed=SpeedConstants.TURN, degrees=406 
                     )
-                    tank_drive.off()
+                    ev3_hardware.tank_drive.off() 
                     wait_for_phone_placed(ws)
                     if ws is not None:
                         message = {"Type": "DRIVE_TO_ROOM_ANSWER", "Answer": "TRUE"}
@@ -106,10 +105,10 @@ def _navigate_in_target_room(target_index, ws=None):
 
 def follow_line_with_green_count(target_count, green_seen, floor_color=None):
     global last_color_green
-    right_color_id = sensor_right.value(0)
+    right_color_id = ev3_hardware.sensor_right.value(0) 
 
     # Nur bei Übergang von Nicht Gruen zu Gruen zählen
-    if right_color_id == GREEN:
+    if right_color_id == ColorValues.GREEN: 
         if not last_color_green:
             green_seen += 1
             print(
@@ -120,7 +119,7 @@ def follow_line_with_green_count(target_count, green_seen, floor_color=None):
         last_color_green = True
         if green_seen == target_count:
             print("Ziel erreicht. Abbiegen.")
-            tank_drive.off()
+            ev3_hardware.tank_drive.off() 
             return TARGET_ROOM_REACHED, green_seen
     else:
         last_color_green = False
@@ -133,10 +132,10 @@ def turn_into_room():
 
     # Folge der Linie bis zur blauen Platte im Raum
     while True:
-        floor_color = sensor_floor.color
-        right_color_id = sensor_right.value(0)
+        floor_color = ev3_hardware.sensor_floor.color
+        right_color_id = ev3_hardware.sensor_right.value(0)
 
-        if right_color_id == BLUE:
+        if right_color_id == ColorValues.BLUE: 
             turn_180_degrees()
             return
         else:
@@ -146,25 +145,25 @@ def turn_180_degrees():
     print(
         "Blaue Platte erkannt - 180 Grad drehen"
     )
-    tank_drive.off()
-    tank_drive.on_for_degrees(
-        left_speed=-SPEED_TURN, right_speed=SPEED_TURN, degrees=406
+    ev3_hardware.tank_drive.off() 
+    ev3_hardware.tank_drive.on_for_degrees( 
+        left_speed=-SpeedConstants.TURN, right_speed=SpeedConstants.TURN, degrees=406 
     )
-    tank_drive.off()
+    ev3_hardware.tank_drive.off() 
     return
 
 def turn_left_90_degrees():
     # Dreht den Roboter nach um 90° nach links, bis er wieder Schwarz erkennt
     print("Drehe 90 Grad nach links")
-    tank_drive.on_for_degrees(
-        left_speed=-SPEED_TURN, right_speed=SPEED_TURN, degrees=203
+    ev3_hardware.tank_drive.on_for_degrees( 
+        left_speed=-SpeedConstants.TURN, right_speed=SpeedConstants.TURN, degrees=203 
     )
-    tank_drive.off()
+    ev3_hardware.tank_drive.off() 
 
 def turn_right_90_degrees():
     # Dreht den Roboter nach um 90° nach links, bis er wieder Schwarz erkennt
     print("Drehe 90 Grad nach rechts")
-    tank_drive.on_for_degrees(
-        left_speed=SPEED_TURN, right_speed=-SPEED_TURN, degrees=203
+    ev3_hardware.tank_drive.on_for_degrees( 
+        left_speed=SpeedConstants.TURN, right_speed=-SpeedConstants.TURN, degrees=203 
     )
-    tank_drive.off()
+    ev3_hardware.tank_drive.off() 
